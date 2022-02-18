882f08398cbffa41c1a001754e0a8cf714b4d6f7
Júlia Bassul <jubassul@gmail.com>
2022-02-18 18:12:39 -0300

    adição do método atualizar status

--- controllers/PedidosController.js
+++ controllers/PedidosController.js
@@ -1,64 +1,69 @@
+const { json } = require("express");
 const Pedidos = require("./../models/Pedidos");
 
 class PedidoController {
+  static async cadastrar(req, res) {
+    const { nome, dataDesejada, endereco, status, nomeEntregador } = req.body;
 
-    static async cadastrar(req, res){
-        const { nome, dataDesejada, endereco, status, nomeEntregador } = req.body
+    const pedido = Pedidos({
+      nome,
+      dataDesejada,
+      endereco,
+      status,
+      nomeEntregador,
+    });
 
-        const pedido = Pedidos( { nome, dataDesejada, endereco, status, nomeEntregador } )
-
-        if(!nome || !dataDesejada || !endereco ||!status ||!nomeEntregador) {
-            res.status(402).json( { message: 'Campos não preenchidos corretamente'} )
-            return
-        }
-        
-        await pedido.save()    
-
-        res.status(201).json( { message: `Pedido ${nome} cadastrado` } )
-
+    if (!nome || !dataDesejada || !endereco || !status || !nomeEntregador) {
+      res.status(402).json({ message: "Campos não preenchidos corretamente" });
+      return;
     }
 
-    static async editar(req, res){
-        const { id } = req.body;
-        
-        if(
-            !id ||
-            !req.body.nome ||
-            !req.body.dataDesejada ||
-            !req.body.endereco ||
-            !req.body.status ||
-            !req.body.nomeEntregador
-        ){
-            res.status(402).json({
-                message: `parametro(s)-necessario(s)-nulo(s)`
-            })
-            return;
-        }
+    await pedido.save();
 
-        const updatePedido = {
-            nome: req.body.nome,
-            dataDesejada: req.body.dataDesejada,
-            endereco: req.body.endereco,
-            status: req.body.status,
-            nomeEntregador: req.body.nomeEntregador
-        }
+    res.status(201).json({ message: `Pedido ${nome} cadastrado` });
+  }
 
-        const pedido = await Pedidos.findById(id);
+  static async editar(req, res) {
+    const { id } = req.body;
 
-        if(!pedido){
-            res.status(406).json({
-                message: 'pedido-inexistente'
-            })
-            return;
-        }
-
-        await Pedidos.findByIdAndUpdate(id, updatePedido);
-
-        res.json({
-            message: `pedido-${id}-atualizado`,
-            pedido: updatePedido
-        })
+    if (
+      !id ||
+      !req.body.nome ||
+      !req.body.dataDesejada ||
+      !req.body.endereco ||
+      !req.body.status ||
+      !req.body.nomeEntregador
+    ) {
+      res.status(402).json({
+        message: `parametro(s)-necessario(s)-nulo(s)`,
+      });
+      return;
     }
+
+    const updatePedido = {
+      nome: req.body.nome,
+      dataDesejada: req.body.dataDesejada,
+      endereco: req.body.endereco,
+      status: req.body.status,
+      nomeEntregador: req.body.nomeEntregador,
+    };
+
+    const pedido = await Pedidos.findById(id);
+
+    if (!pedido) {
+      res.status(406).json({
+        message: "pedido-inexistente",
+      });
+      return;
+    }
+
+    await Pedidos.findByIdAndUpdate(id, updatePedido);
+
+    res.json({
+      message: `pedido-${id}-atualizado`,
+      pedido: updatePedido,
+    });
+  }
 
   static async visualizar(req, res) {
     const pedido = await Pedidos.find().lean();
@@ -73,7 +78,26 @@ class PedidoController {
 
   static async verStatus(req, res) {}
 
-  static async attStatus(req, res) {}
+  static async attStatus(req, res) {
+    const id = req.body.id;
+    const pedido = {
+      status: req.body.status,
+    };
+    if (!id) {
+      res.status(402).json({ message: "Parâmetro id nulo" });
+      return;
+    }
+    const procurarAttpedido = await Pedidos.findByIdAndUpdate(id, pedido);
+    if (!procurarAttpedido) {
+      res.status(402).json({ message: "Pedido não encontrado" });
+      return;
+    }
+    if (!pedido.status) {
+      res.status(402).json({ message: "Parâmetro status nulo" });
+      return;
+    }
+    res.status(200).json({ status: `Status alterado para ${pedido.status}` });
+  }
 }
 
 module.exports = PedidoController;
