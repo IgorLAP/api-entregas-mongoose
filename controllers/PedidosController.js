1238daf74f73d7eb55000f409f5eb8577096adcc
Júlia Bassul <jubassul@gmail.com>
2022-02-18 15:54:29 -0300

    Adição do método visualizar produtos

--- controllers/PedidosController.js
+++ controllers/PedidosController.js
@@ -1,31 +1,24 @@
-const Produto = require('./../models/Pedidos');
+const Pedidos = require("./../models/Pedidos");
 
-class PedidoController{
-    static async cadastrar(req, res){
+class PedidoController {
+  static async cadastrar(req, res) {}
 
+  static async editar(req, res) {}
+
+  static async visualizar(req, res) {
+    const pedido = await Pedidos.find().lean();
+    if (!pedido) {
+      res.status(402).json({ message: "lista-pedido-nulo" });
+      return;
     }
+    res.status(202).json(pedido);
+  }
 
-    static async editar(req, res){
+  static async deletar(req, res) {}
 
-    }
+  static async verStatus(req, res) {}
 
-    static async visualizar(req, res){
-
-    }
-
-    static async deletar(req, res){
-
-    }
-
-    static async verStatus(req, res){
-
-    }
-
-    static async attStatus(req, res){
-
-    }
-
+  static async attStatus(req, res) {}
 }
-
 
 module.exports = PedidoController;
